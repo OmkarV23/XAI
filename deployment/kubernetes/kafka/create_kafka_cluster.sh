f10e554d829e9c962de4d4d58bfdb0091171c7a4
Shikhar Gupta <sgupt330@asu.edu>
2023-07-22 22:30:50 -0700

    Fixed mongo directory issue

--- deployment/kubernetes/kafka/create_kafka_cluster.sh
+++ deployment/kubernetes/kafka/create_kafka_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f persistent-volume/kafka-storage.yml
+kubectl apply -f kafka-storage.yml
 
 if [[ $(helm repo list | grep rhcharts) ]]; then
     echo "Repo already added"
@@ -8,4 +8,4 @@ else
     helm repo add rhcharts https://ricardo-aires.github.io/helm-charts/
 fi
 
-helm install kafka -f helm-chart/values.yaml rhcharts/kafka
+helm install kafka -f kafka-config-values.yaml rhcharts/kafka
