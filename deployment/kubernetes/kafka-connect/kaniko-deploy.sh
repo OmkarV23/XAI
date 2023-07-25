48738f19535f739e51db02ee7c9f67f52a2fa7fd
Shikhar Gupta <sgupt330@asu.edu>
2023-07-25 00:26:39 -0700

    Added kaniko deployment config

--- deployment/kubernetes/kafka-connect/kaniko-deploy.sh
+++ deployment/kubernetes/kafka-connect/kaniko-deploy.sh
@@ -1,5 +1,3 @@
-kubectl create secret docker-registry dockercred \
---docker-server=https://index.docker.io/v1/ \
---docker-username=shikharg1997 \
---docker-password=Pesu@2015\
---docker-email=sgupt330@asu.edu
+#!/bin/bash
+
+kubectl create secret docker-registry dockercred --docker-server=https://index.docker.io/v1/ --docker-username="shikharg1997" --docker-password="Pesu@2015" --docker-email="sgupt330@asu.edu"
