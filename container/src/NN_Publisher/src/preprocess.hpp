dd013db748123d0f57d16080663ccbf206f122fa
OmkarV23 <75058026+OmkarV23@users.noreply.github.com>
2023-08-17 21:29:41 -0700

    Update preprocess.hpp

--- container/src/NN_Publisher/src/preprocess.hpp
+++ container/src/NN_Publisher/src/preprocess.hpp
@@ -39,20 +39,20 @@ PreprocessedData preprocess(cv::Mat image, bool flag=false)
     // cv:imshow("image", image);
 
     cv::cvtColor(image, ImageRGB, cv::ColorConversionCodes::COLOR_BGR2RGB);
-    cv::resize(ImageRGB, ImageRGB, cv::Size(224, 224), cv::InterpolationFlags::INTER_LINEAR);
+    cv::resize(ImageRGB, ImageRGB, cv::Size(256, 256), cv::InterpolationFlags::INTER_LINEAR);
     
     // transposedimage = convertNHWCtoNCHW(ImageRGB);
 
     ImageRGB.convertTo(resizedImage, CV_32FC3, 1.0 / 255.0);
 
-    // cv::Mat channels[3];
-    // cv::split(resizedImage, channels);
+    cv::Mat channels[3];
+    cv::split(resizedImage, channels);
 
-    // channels[0] = (channels[0] - 0.485) / 0.229;
-    // channels[1] = (channels[1] - 0.456) / 0.224;
-    // channels[2] = (channels[2] - 0.406) / 0.225;
+    channels[0] = (channels[0] - 0.485) / 0.229;
+    channels[1] = (channels[1] - 0.456) / 0.224;
+    channels[2] = (channels[2] - 0.406) / 0.225;
 
-    // cv::merge(channels, 3, resizedImage);
+    cv::merge(channels, 3, resizedImage);
 
     cv::dnn::blobFromImage(resizedImage, preprocessedImage);
 
